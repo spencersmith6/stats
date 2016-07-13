a476c6c00e513b188a1c3a36e3ce9deefccae0f3
Spencer Smith <ssmith22@usfca.edu>
2016-07-13 11:01:40 -0700

    Added function for normal pdf

--- conf.py
+++ conf.py
@@ -5,7 +5,7 @@ import scipy.stats as stats
 import sys
 
 fancy = False
-TRIALS = 40
+TRIALS = 500
 if len(sys.argv)>1:
     TRIALS = int(sys.argv[1])
 
@@ -37,6 +37,13 @@ def avg(set):
     avg = sum(set) / len(set)
     return avg
 
+def normpdf(x,mean,sd):
+    out = (1/(sd*(np.sqrt(2*np.pi))))*(np.e**((-((x-mean)**2))/float((2*(sd**2)))))
+    return out
+
+print normpdf(1,0,1)
+
+print np.pi
 
 sorted = sorted(trials(TRIALS))
 
@@ -62,6 +69,9 @@ x = np.arange(1.05, 1.25, .001 )
 pl.axis([1.10,1.201,0,30])
 x = np.arange(1.05, 1.25, .001 )
 y = stats.norm.pdf(x,mean,sd)
+#y = normpdf(x,mean,sd)
+
+
 pl.plot(x,y,color='red')
 
 z = [l, r]
@@ -72,7 +82,7 @@ left = (mean - (1.96*sd))
 right = (mean + (1.96*sd))
 
 ci_x = np.arange(left, right, .001)
-ci_y = stats.norm.pdf(ci_x, mean, sd)
+ci_y = normpdf(ci_x, mean, sd)
 pl.fill_between(ci_x, ci_y, color = '#F8ECE0')
 
 pl.text(.02,.95,'$TRIALS = %d$' % TRIALS, transform = ax.transAxes)
@@ -89,7 +99,7 @@ pl.title("95% Confidence Intervals: $\\mu \\pm 1.96\\sigma$", fontsize=16)
 
 ax.annotate("Empirical 95% CI",xy=(inside[0], .3),xycoords="data",xytext=(1.13, 4), textcoords='data', arrowprops = dict(arrowstyle="->",connectionstyle="arc3"),fontsize = 16)
 
-#pl.savefig('conf-'+str(TRIALS)+('basic' if not fancy else'')+'.pdf', format='pdf')
+pl.savefig('conf-'+str(TRIALS)+('basic' if not fancy else'')+'.pdf', format='pdf')
 
 pl.show()
 
